b137dae18983555aa1d9d995f36cd9795a8fa7b6
Tsvety-Gerova <71559796+Tsvety-Gerova@users.noreply.github.com>
2020-10-01 17:26:36 +0300

    Add files via upload

---
+++ project07/src/project07/Tution.java
@@ -0,0 +1,16 @@
+package project07;
+import java.util.Scanner;
+public class Tution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int tution = 10000;
+for (int i = 0; i < 10; i++){
+	tution *= 106/100; 
+	}
+System.out.println("The tution in ten years will be"  +  tution);
+
+
+	}
+
+}
